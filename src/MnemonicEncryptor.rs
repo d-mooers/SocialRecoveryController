b3dbd3598ac89b6d2bc65033c5b09716cd414187
Dylan Mooers <dylmooers@gmail.com>
2022-01-31 08:55:12 -0800

    decrypt mnemonic function

--- src/MnemonicEncryptor.rs
+++ src/MnemonicEncryptor.rs
@@ -3,10 +3,11 @@ use aes_gcm::{Aes256Gcm, Key, Nonce};
 use aes_gcm::aead::{Aead, NewAead};
 use rsa::{PublicKey, RsaPrivateKey, PaddingScheme};
 use rand::Rng;
+// DATA_SIZE = 64 (in shamirsecretsharing)
+const NONCE_SIZE = 12;
+const KEY_SIZE = DATA_SIZE - NONCE_SIZE;
 
 fn encrypt_mnemonic(mnemonic: &str) {
-    const NONCE_SIZE = 12;
-    const KEY_SIZE = DATA_SIZE - NONCE_SIZE;
     let mut rng = rand::thread_rng();
     let key_and_nonce: Vec<u8> = Vec::with_capacity(DATA_SIZE);
     let nonce: Nonce = Nonce::from_slice()
@@ -19,6 +20,13 @@ fn encrypt_mnemonic(mnemonic: &str) {
     let cipher = Aes256Gcm::new(key);
     let encrypted_mnemonic = cipher.encrypt(nonce, mnemonic).expect("encryption failure!");
     return (key_and_nonce, encrypted_mnemonic);
+}
+
+fn decrypt_mnemonic(encrypted_mnemonic: &[u8], key_and_nonce: &[u8]) {
+    let key = Key::from_slice(key_and_nonce[0..KEY_SIZE]);
+    let nonce = Nonce::from_slice(key_and_nonce[KEY_SIZE..DATA_SIZE]);
+    let cipher = Aes256Gcm::new(key);
+    return cipher.decrypt(nonce, encrypted_mnemonic).expect("decryption failure!")
 }
 
 fn create_key_shares(secret: &[u8], n: u8, k: u8) -> Vector<Vector<u8>> {
